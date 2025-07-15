91cbec9b4a134e1ff6185fd26798586eee25f603
Valberton <valberton77@gmail.com>
2025-07-15 13:19:01 -0300

    added project's base

---
+++ src/main/java/com/proj/bfftaskscheduler/BffTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.proj.bfftaskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffTaskSchedulerApplication.class, args);
+	}
+
+}
